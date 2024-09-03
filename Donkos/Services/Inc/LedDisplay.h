8c77162f4c4cba38090be45ef8eac09a17a37ac6
Ludwig Kratzl <ludwig.kratzl@tum.de>
2024-09-03 21:15:56 +0200

    Prepare LED display

---
+++ Donkos/Services/Inc/LedDisplay.h
@@ -0,0 +1,25 @@
+//
+// Created by ludwig on 9/3/24.
+//
+
+#ifndef TEST_LEDDISPLAY_H
+#define TEST_LEDDISPLAY_H
+
+#include "main.h"
+
+class LedDisplay {
+public:
+    LedDisplay();
+
+    void Init();
+
+    void Display(const char *text);
+
+    void Refresh();
+
+private:
+    I2C_HandleTypeDef hi2c2;
+};
+
+
+#endif //TEST_LEDDISPLAY_H
