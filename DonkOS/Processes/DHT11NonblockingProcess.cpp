48a670720b65277ecddb2c8df679d8be0e99227f
Ludwig Kratzl <ludwig.kratzl@tum.de>
2025-08-27 18:32:08 +0200

    Fix non-blocking timer with timer-based sampling

--- DonkOS/Processes/DHT11NonblockingProcess.cpp
+++ DonkOS/Processes/DHT11NonblockingProcess.cpp
@@ -1,3 +1,4 @@
+#include <cstring>
 #include "DHT11NonblockingProcess.h"
 #include "DonkosInternal.h"
 #include "StringConverter.h"
@@ -11,13 +12,19 @@ void DHT11NonblockingProcess::SetHandle(TIM_HandleTypeDef handle) {
 }
 
 uint32_t bits_received = 0;
-uint64_t data_received = 0;
+uint8_t data_received[5];
+int32_t interrupt_offset = 0;
+int32_t balance = 0;
+bool clear = true;
+
+uint8_t indices[40] = {0};
 
 void DHT11NonblockingProcess::Main() {
     while (true) {
         if (state == DHT_STATE::RESTART) {
             bits_received = 0;
-            data_received = 0;
+            data_received[0] = data_received[1] = data_received[2] = data_received[3] = data_received[4] = 0;
+            std::memset(&indices, 0, 40);
             HAL_GPIO_WritePin(DHT_DATA_GPIO_Port, DHT_DATA_Pin, GPIO_PIN_SET);
             wait(1000);
             state = DHT_STATE::IDLE;
@@ -37,14 +44,15 @@ void DHT11NonblockingProcess::Main() {
         }
 
         if (state == DHT_STATE::PROCESS_DATA) {
-
-            uint32_t hum1 = data_received & 0xFF00000000 >> 32;
-            uint32_t hum2 = data_received & 0x00FF000000 >> 24;
-            uint32_t temp1 = data_received & 0x0000FF0000 >> 16;
-            uint32_t temp2 = data_received & 0x000000FF00 >> 8;
-            uint32_t check = data_received & 0x00000000FF >> 0;
+            uint32_t hum1 = data_received[0];
+            uint32_t hum2 = data_received[1];
+            uint32_t temp1 = data_received[2];
+            uint32_t temp2 = data_received[3];
+            uint32_t check = data_received[4];
 
             if (check == hum1 + hum2 + temp1 + temp2) {
+                balance++;
+
                 StringConverter converter{};
                 char output_temperature_string[11] = "T: ";
                 char output_humidity_string[11] = "H: ";
@@ -78,10 +86,14 @@ void DHT11NonblockingProcess::Main() {
             }
 
         }
-
         if (state == DHT_STATE::COMM_ERROR) {
-            Donkos_Display(2, 2, "Fehler :(");
-            Donkos_Display(2, 3, "");
+            balance--;
+
+            if (balance < -10) {
+                Donkos_ClearDisplay();
+                Donkos_Display(2, 2, "Fehler :(");
+                Donkos_Display(2, 3, "");
+            }
 
             state = DHT_STATE::RESTART;
         }
@@ -94,76 +106,85 @@ void DHT11NonblockingProcess::TimerInterruptReceived() {
     __HAL_TIM_DISABLE(&htim);
     __HAL_TIM_SET_COUNTER(&htim, 0);
 
-    int32_t new_time = 0;
 
-    // // called after 18 ms signal low -> now set to high, expect DHT to work immediately
+    int32_t init_low_phase = 84;
+    int32_t init_high_phase = 90;
+    int32_t data_low_time = 26;
+    int32_t data_high_time = 78;
+    int32_t data_pause = 52;
+
+    int32_t new_time = 0;
+    interrupt_offset = 5;
+
+    // // called after 18 ms signal low -> now set to high, expect DHT to work immediately by setting low ~84 us
     if (state == DHT_STATE::COMM_INITIALIZED) {
         HAL_GPIO_WritePin(DHT_DATA_GPIO_Port, DHT_DATA_Pin, GPIO_PIN_SET);
         state = DHT_STATE::COMM_INITIALIZED_WAIT;
-        new_time = 50;
+        new_time = init_low_phase / 2;
     }
-        // called after 54 us data high. device should have set pin low by now -> now device should set 80 us high
+        // device should have set pin low by now -> now device should set ~90 us high next
     else if (state == DHT_STATE::COMM_INITIALIZED_WAIT) {
         // now: data coming from dht
         if (HAL_GPIO_ReadPin(DHT_DATA_GPIO_Port, DHT_DATA_Pin) == GPIO_PIN_RESET) {
             state = DHT_STATE::COMM_INITIALIZED_WAIT_FOR_DEVICE;
-            new_time = 80;
+            new_time = init_high_phase / 2 + init_low_phase / 2;
 
         } else {
             state = DHT_STATE::COMM_ERROR;
         }
     }
-        // called after 80 us. device should set pin to high -> now start read data (starts with 50 us low signal. split in two halves for a check)
+        // device should have set pin to high. next is low phase for 50 us
     else if (state == DHT_STATE::COMM_INITIALIZED_WAIT_FOR_DEVICE) {
 
         if (HAL_GPIO_ReadPin(DHT_DATA_GPIO_Port, DHT_DATA_Pin) == GPIO_PIN_SET) {
             state = DHT_STATE::READY_FOR_BIT;
-            new_time = 58;
+            new_time = data_pause / 2 + init_high_phase / 2;
         } else {
             state = DHT_STATE::COMM_ERROR;
         }
     }
-        // intermediate check - has the signal been set to low? if yes we can start continue waiting
+        //next bit is high but pulse is of variable length (30 or 70 us)
     else if (state == DHT_STATE::READY_FOR_BIT) {
-        if (HAL_GPIO_ReadPin(DHT_DATA_GPIO_Port, DHT_DATA_Pin) == GPIO_PIN_RESET) {
-            state = DHT_STATE::READ_BIT;
-            new_time = 30;
-        } else {
-            state = DHT_STATE::COMM_ERROR;
-        }
+        state = DHT_STATE::WAIT_FOR_BIT;
+        new_time = data_low_time / 2 + data_pause / 2;
     }
-        // successfully waited 50 us on low. now we can start read. check after 30 us again
+        // initiate next wait if high was read
     else if (state == DHT_STATE::WAIT_FOR_BIT) {
-        if (HAL_GPIO_ReadPin(DHT_DATA_GPIO_Port, DHT_DATA_Pin) == GPIO_PIN_SET) {
-            state = DHT_STATE::READ_BIT;
-            new_time = 30;
-        } else {
-            state = DHT_STATE::COMM_ERROR;
-        }
+        state = DHT_STATE::READ_BIT;
+        new_time = data_pause / 2 + data_low_time / 2;
     }
         // bit arrived - after 30 us. if lane is low, bit is low, otherwise wait longer and it is high
         // after read, wait 25 us for the intermediate check above if more bits expected
     else if (state == DHT_STATE::READ_BIT) {
         int32_t next_wait = 0;
-        bits_received++;
 
         if (HAL_GPIO_ReadPin(DHT_DATA_GPIO_Port, DHT_DATA_Pin) == GPIO_PIN_RESET) {
-            data_received <<= 1;
-            next_wait = 25 / 2 + 56 / 2;
+            next_wait = data_pause / 2 + data_low_time / 2;
+            indices[bits_received] = 0;
+
+            state = DHT_STATE::WAIT_FOR_BIT;
+
         } else {
-            data_received = (data_received | 1) << 1;
-            next_wait = 25 / 2 + 56 / 2 + 30;
+            auto index = bits_received / 8;
+            auto shift = bits_received % 8;
+
+            data_received[index] |= (1 << (7 - shift));
+            next_wait = (data_high_time - data_low_time / 2 - data_pause / 2);
+            indices[bits_received] = 1;
+            interrupt_offset = -3;
+
+            state = DHT_STATE::READY_FOR_BIT;
         }
+        bits_received++;
 
         if (bits_received < 40) {
-            state = DHT_STATE::READ_BIT;
-            new_time = (next_wait);
+            new_time = next_wait;
 
         } else {
             state = DHT_STATE::COMM_END;
             HAL_GPIO_WritePin(DHT_DATA_GPIO_Port, DHT_DATA_Pin, GPIO_PIN_RESET);
             // end comm by setting lane to low
-            new_time = (50 + 56 / 2);
+            new_time = 50;
         }
     }
         // comm ended -> now data can be processed
@@ -174,7 +195,7 @@ void DHT11NonblockingProcess::TimerInterruptReceived() {
 
     if (new_time > 0) {
         // new AAR !! auto reload enable bit must be set
-        __HAL_TIM_SET_AUTORELOAD(&htim, new_time - 1);
+        __HAL_TIM_SET_AUTORELOAD(&htim, new_time - 1 - interrupt_offset);
         htim.Instance->EGR = TIM_EGR_UG;
         __HAL_TIM_CLEAR_IT(&htim, TIM_DIER_UIE);
 
